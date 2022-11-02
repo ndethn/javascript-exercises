3bc72a9b54d6195698a5f6c9dc6f62188cfd1848
Ethan <uhhhhhethan@gmail.com>
2022-11-02 10:26:50 -0700

    Create function to remove from array attempt 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, value, ...theArgs) {
+    // declare the rest parameter into a variable
+    let argsValue = theArgs;
+    // declare for loop to iterate through each index of arr
+    for (let i = 0; i < arr.length; i++) {
+        // if value matches the element index -> remove it
+        if (arr[i] === value) {
+            arr.splice(i, 1);
+            i--;
+        // loop through the rest param array...
+        } else if (arr[i] === argsValue) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
