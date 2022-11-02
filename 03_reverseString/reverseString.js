553fc6615fa31b4da5d486aa8b24af3c71da216e
Ethan <uhhhhhethan@gmail.com>
2022-11-02 09:31:24 -0700

    Remove loop and incorporate string methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,6 @@
 const reverseString = function (string) {
-    let newString = ``;
-    for (let i = string.length - 1; i >= 0; i--) {
-        newString += string[i];
-    }
-    return newString;
+    return string.split('').reverse().join('');
 };
-reverseString(`hello`);
 
 // Do not edit below this line
 module.exports = reverseString;
