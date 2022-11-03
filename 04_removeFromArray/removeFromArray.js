fe499f7e8b959ba0ee0161755850ac77de343f26
Ethan <uhhhhhethan@gmail.com>
2022-11-03 09:46:08 -0700

    Try nesting a for loop and adjusting the parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,22 @@
-const removeFromArray = function (arr, value, ...theArgs) {
-    // declare the rest parameter into a variable
-    let argsValue = theArgs;
+const removeFromArray = function (...theArgs) {
+    // declare the rest parameter into an array
+    const argsValue = theArgs;
     // declare for loop to iterate through each index of arr
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < argsValue.length; i++) {
         // if value matches the element index -> remove it
-        if (arr[i] === value) {
-            arr.splice(i, 1);
+        if (argsValue[i] === argsValue) {
+            argsValue.splice(i, 1);
             i--;
-        // loop through the rest param array...
-        } else if (arr[i] === argsValue) {
-            arr.splice(i, 1);
-            i--;
+            // loop through the args arr
+            for (let j = 0; j < argsValue.length; j++) {
+                if (argsValue[i] === argsValue[i]) {
+                    argsValue.splice(i, 1);
+                    i--;
+                }
+            }
         }
     }
-    return arr;
+    return argsValue;
 };
 
 // Do not edit below this line
