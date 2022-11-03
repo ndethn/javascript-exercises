d09d5d674cf20c7c96e1271ac37b103e39cc2e1f
Ethan <uhhhhhethan@gmail.com>
2022-11-03 11:22:41 -0700

    Rearrange code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,13 @@
 // set parameters as val1 value and max value
 const sumAll = function (val1, val2) {
-    // declare total variable
-    let totalSum = 0;
     // condition for non numbers
     if (!Number.isInteger(val1) || !Number.isInteger(val2)) return "ERROR";
     // condition for negative numbers
     if (val1 < 0 || val2 < 0) {
         return "ERROR";
     }
+    // declare total variable
+    let totalSum = 0;
     // condition if val2 is greater
     if (val1 < val2) {
         // use a for loop to increment between val1 and val2
