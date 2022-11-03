2df780f63f5f66c54dec0e455c44c385d8672f6d
Ethan <uhhhhhethan@gmail.com>
2022-11-03 10:02:54 -0700

    Add comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -16,5 +16,14 @@ const removeFromArray = function (...theArgs) {
     return newArray;
 };
 
+// A simpler, but more advanced way to do it is to use the 'filter' function,
+// which basically does what we did with the forEach above.
+
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
+//
+
 // Do not edit below this line
 module.exports = removeFromArray;
