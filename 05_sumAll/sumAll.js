a872da74944fe29e1be590da5b7cf4999e66832a
Ethan <uhhhhhethan@gmail.com>
2022-11-03 10:50:49 -0700

    Write code block for sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+// set parameters as min value and max value
+const sumAll = function(min, max) {
+    // declare total variable
+    let totalSum = 0;
+    // use a for loop to increment between min and max
+    // to grab each number between min and max
+    for (let i = min - 1; i <= max; i++){
+        totalSum += i;
+    }
+    // return total
+    return totalSum;
 };
 
 // Do not edit below this line
