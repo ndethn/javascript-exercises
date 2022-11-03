db966015345cebed570db4e1db11d8737f083b93
Ethan <uhhhhhethan@gmail.com>
2022-11-03 11:22:01 -0700

    Modify for loop statements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -12,12 +12,12 @@ const sumAll = function (val1, val2) {
     if (val1 < val2) {
         // use a for loop to increment between val1 and val2
         // to grab each number between val1 and val2
-        for (let i = val1 - 1; i <= val2; i++) {
+        for (let i = val1; i < val2 + 1; i++) {
             totalSum += i;
         }
         // condition if val1 is greater
     } else {
-        for (let i = val2 - 1; i <= val1; i++) {
+        for (let i = val2; i < val1 + 1; i++) {
             totalSum += i;
         }
     }
