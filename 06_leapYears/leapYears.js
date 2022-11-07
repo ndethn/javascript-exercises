f77181be4d684550553411975e22e47d1d1a4853
Ethan <uhhhhhethan@gmail.com>
2022-11-07 10:57:48 -0800

    Modify if statement to one line

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,5 @@
 const leapYears = function (year) {
-    if (year % 4 == 0 && year % 100 == 0) {
-        return false;
-    } else if (year % 4 == 0) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
         return true;
     }
     return false;
