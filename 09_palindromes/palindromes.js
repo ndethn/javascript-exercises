d696ceb884f24cd11dade518269264c6ca58ddea
Ethan <uhhhhhethan@gmail.com>
2022-12-05 12:23:31 -0800

    Used reg ex for loop for palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
 const palindromes = function (str) {
+    let re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+
+    let l = str.length;
+    for (let i = 0; i < l / 2; i++) {
+        if (str[i] !== str[l-1-i]) {
+            return false;
+        }
+    }
     return true;
 };
 
