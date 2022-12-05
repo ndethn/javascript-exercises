7f688a64490b2387ddc88a28ef0e8747078b031f
Ethan <uhhhhhethan@gmail.com>
2022-12-05 12:39:57 -0800

    Add two ways to check for palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,18 @@
 const palindromes = function (str) {
     let re = /[^A-Za-z0-9]/g;
-    str = str.toLowerCase().replace(re, '');
+    // str = str.toLowerCase().replace(re, '');
 
-    let l = str.length;
-    for (let i = 0; i < l / 2; i++) {
-        if (str[i] !== str[l-1-i]) {
-            return false;
-        }
-    }
-    return true;
+    // let l = str.length;
+    // for (let i = 0; i < l / 2; i++) {
+    //     if (str[i] !== str[l-1-i]) {
+    //         return false;
+    //     }
+    // }
+    // return true;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
+
 };
 
 // Do not edit below this line
