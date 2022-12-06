034969242505c1c90ce357bdea44d2e77a379063
Ethan <uhhhhhethan@gmail.com>
2022-12-06 11:24:52 -0800

    Create fibonacci function in two different ways

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // return num < 1 ? "OOPS"
+    // : num <= 2 ? 1
+    // : fibonacci(num - 1) + fibonacci(num - 2)
 
+    if (num < 1) return "OOPS";
+    if (num === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < num; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
